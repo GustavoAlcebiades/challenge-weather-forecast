4e0cc8114b4a217017d82aba96eb72d42cd64d89
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2020-03-11 00:14:17 -0300

    Desenvolvido para absorver a API openWeatherMap com jQuery ajax e JavaScript

---
+++ weather.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    
+    $('#submitWeather').click(function(){
+        
+        var city = $("#city").val();
+        
+        if(city != ''){
+            
+            $.ajax({
+               url:'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=69e72bf62be00fc3554ae91904da84e2",
+                type: "GET",
+                dataType:"jsonp",
+                success: function(data){
+                    var widget = show(data);
+               
+                    $("#show").html(widget);
+                    
+                    $("#city").val('');
+                    
+                }
+                
+                
+                
+            });
+            
+            
+        }else{
+            $("#error").html("<div class = 'alert alert-danger' id='cityError'><a href ='#' class = 'close' data-dismiss='alert' aria-label = 'close'>&times;  </a> Here cannot be empty</empty>");
+        }
+        
+    });
+});
+
+
+function show(data){
+    return  "<h2>Current Weather for " + data.name + "," + data.sys.country + "</h2>" + 
+            "<h3><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+            "<h3><strong>Description</strong>: <img src='http://openweathermap.org/img/wn/"+data.weather[0].icon+".png'> " + data.weather[0].description + "</h3>" +
+            "<h3><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" + 
+            "<h3><strong>Pressure</strong>: " + data.main.pressure + "hPa</h3>" + 
+            "<h3><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+            "<h3><strong>Temperature min</strong>: " + data.main.temp_min + "&deg;C</h3>" + 
+            "<h3><strong>Temperature max</strong>: " + data.main.temp_max + "&deg;C</h3>" + 
+            "<h3><strong>Wind speed:</strong>: " + data.wind.speed + "m/s</h3>";
+    
+}
